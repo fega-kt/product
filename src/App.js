7c41da1bff6e102cd088852dcaae39daabb34319
fe gà <fega.kt@gmail.com>
2022-11-20 21:18:57 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import  Checkinternet  from "./component/Checkinternet";
+import { Link, Route, Routes } from "react-router-dom";
+import Checkinternet from "./component/Checkinternet";
+import LoginPage from "./component/LoginPage";
+import DashboardHome from "./component/DashboardHome";
 function App() {
   return (
     <>
       <Checkinternet>
-        <div className="App">kim thasi</div>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to={"/"}> home</Link>
+            </li>
+            <li>
+              <Link to={"/login"}> login</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<DashboardHome />} />
+        </Routes>
       </Checkinternet>
     </>
   );
