81edb5b0bb1a95940498bee37c6c0f4b2ca84930
fe gà <fega.kt@gmail.com>
2022-11-20 12:47:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  Checkinternet  from "./component/Checkinternet";
+function App() {
+  return (
+    <>
+      <Checkinternet>
+        <div className="App">kim thasi</div>
+      </Checkinternet>
+    </>
+  );
+}
+
+export default App;
