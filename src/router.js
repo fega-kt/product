5c81f3cb16421c9fd0cb35426711c59532cb62a8
dev 21 <dev21@lifetek.vn>
2022-11-21 14:44:34 +0700

    update router

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import DashboardHome from "./component/DashboardHome";
+const routes = [
+  {
+    path: "/incommingDocument",
+    exact: true,
+    name: "văn bản đển",
+    component: DashboardHome,
+  },
+];
+export default routes;
