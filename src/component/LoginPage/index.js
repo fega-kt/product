f163ec7ad401f0fb66c05d1917df51ce3d924bba
fe gà <fega.kt@gmail.com>
2023-02-07 00:14:27 -0800

    update

--- src/component/LoginPage/index.js
+++ src/component/LoginPage/index.js
@@ -1,9 +1,18 @@
-import { Button, Card, CardActions, CardContent, Grid, Paper, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Grid, InputAdornment, Paper, TextField, Typography } from "@mui/material";
 import React from "react";
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { Helmet } from 'react-helmet';
 import './style.scss'
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
 function LoginPage() {
   console.log(`${process.env.REACT_APP_TITLE_NAME}`, "process")
+  const classes = useStyles();
 
   return <>
    <Helmet>
@@ -11,10 +20,23 @@ function LoginPage() {
     </Helmet>
 
    <Grid item id={"mainLoginPage"} container>
-   <Grid item  md={6} xs={6} className="login">
+   <Grid item  md={4} xs={4}  sm ={6} className="login">
       <Paper elevation={3}className="paper">
       { process.env.REACT_APP_PROJECT_NAME }
-
+      <Grid>
+      <TextField
+        className={classes.margin}
+        id="input-with-icon-textfield"
+        label="TextField"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+      />
+      </Grid>
       </Paper>
 
     </Grid>
